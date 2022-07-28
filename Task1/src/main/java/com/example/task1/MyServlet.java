da510193de6f1168c770b49c4cf679428230d40a
thuggeelya <ilya-fdmdh@mail.ru>
2022-07-28 15:53:36 +0700

    Task1

---
+++ Task1/src/main/java/com/example/task1/MyServlet.java
@@ -0,0 +1,36 @@
+package com.example.task1;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class MyServlet extends HttpServlet {
+
+    private String name;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        name = request.getParameter("name");
+        request.setAttribute("name", name);
+        doGet(request, response);
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+
+        if ((name == null) || (name.trim().isEmpty())) {
+            out.println("<h2>Error: the name is null ..</h2>");
+        } else {
+            out.println("<h1>Hello, " + name + "</h1>");
+        }
+
+        out.println("</body></html>");
+    }
+}
---
+++ Task1/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Main page</title>
+</head>
+<body>
+<form method="post" action="hello">
+    <p>Enter your name:</p>
+    <label>
+        <input type="text" name="name">
+    </label>
+    <input type="submit" value="OK">
+</form>
+</body>
+</html>
